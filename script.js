f0b75153d9b06ba0eb87a568c766823f91895f7f
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-01-31 19:26:27 +0000

    Polishing

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navBar = document.getElementById("nav");
-  
-    function updateNavBarTransparency() {
+  const navBar = document.getElementById("nav");
+  const mobileMenuButton = document.getElementById("mobile-menu-button");
+
+  function updateNavBarTransparency() {
       if (window.scrollY > 50) {
-        navBar.classList.add("transparent");
+          navBar.classList.add("transparent");
       } else {
-        navBar.classList.remove("transparent");
+          navBar.classList.remove("transparent");
       }
-    }
-  
-    updateNavBarTransparency();
-  
-    window.addEventListener("scroll", updateNavBarTransparency);
+  }
+
+  updateNavBarTransparency();
+
+  window.addEventListener("scroll", updateNavBarTransparency);
+
+  function handleResize() {
+      if (window.innerWidth <= 800) {
+
+          navBar.classList.add("hide-mobile");
+      } else {
+
+          navBar.classList.remove("hide-mobile");
+      }
+  }
+
+  window.addEventListener("resize", handleResize);
+
+
+  mobileMenuButton.addEventListener("click", function () {
+      navBar.classList.toggle("hide-mobile");
   });
+
+ 
+  handleResize();
+});
