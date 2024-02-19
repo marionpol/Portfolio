0187df42e5fd709437e572cbd0305f6b7e6490d3
Marion Poljakov <147153357+marionpol@users.noreply.github.com>
2024-02-19 15:54:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ document.addEventListener("DOMContentLoaded", function () {
  
   handleResize();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
+
+
+    mobileMenuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+
+
+    const navLinks = document.querySelectorAll('.nav-menu a');
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function(event) {
+            if (navLink.textContent.trim() === "My resume") {
+
+                if(/Mobi/i.test(navigator.userAgent) && !/iPad|Tablet/i.test(navigator.userAgent)) {
+                    event.preventDefault();
+                    window.open('images/CV_Marion_Poljakov.pdf', '_blank');
+                }
+            } else {
+                navMenu.classList.remove('active');
+            }
+        });
+    });
+});
+
